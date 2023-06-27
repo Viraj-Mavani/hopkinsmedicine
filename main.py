cb3a0e3650daea0d0600a6b61a6ad34dc765bbdc
Viraj Mavani <66798733+Viraj-Mavani@users.noreply.github.com>
2023-06-27 21:32:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
             # Write the headers to the CSV file if it's empty
             writer.writerow(headers)
             
-    # List of user agents to prevant detecting the device and browser
+    # List of user agents to prevent detecting the device and browser
     user_agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
